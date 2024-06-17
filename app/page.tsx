a50cfb54fc39dbd4adfe892b7a84e79db6a65456
Captain Unknown <79987654+CaptainUnknown@users.noreply.github.com>
2024-06-17 14:50:09 +0500

    🔧 Updated the description

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
                     <div className='description'>
                         <h3><a href="https://whoopdoop.com" target="_blank">Whoopdoop Labs</a></h3>
                         <h1>A Raffle System</h1>
-                        <p> Developing an efficient raffle system for ERC-721 tokens that seamlessly handles multiple concurrent raffles.</p>
+                        <p> Designed & developed a cross chain raffle system for ERC-721 tokens, that facilitated over 100k ticket purchases. </p>
                         <OpenButton title='Read Case Study' href='raffle-case-study'/>
                     </div>
                     <Image className='splash' src='/raffleCard.webp' alt='Raffle Cards' width={2810/*2635*/} height={1600} style={{aspectRatio: '2810/1600' }}/>
@@ -41,7 +41,7 @@ export default function Home() {
                     <div className='description'>
                         <h3><a href="https://omibio.netlify.app/" target="_blank">Omibio</a></h3>
                         <h1>A Web3 Music Aggregator</h1>
-                        <p> ⚠️ WIP: Developing a Music Distribution Service that allows artists to mint their songs as on-chain trade-able tokens.</p>
+                        <p> ⚠️ WIP: Developed a Music Distribution Service that allows artists to mint their songs as on-chain trade-able tokens.</p>
                         <OpenButton title='Read Case Study' href='omibio-case-study' />
                     </div>
                     <Image className='omibio' src='/omibio.webp' alt='Omibio App Screens' width={3188/*2635*/} height={2048} style={{aspectRatio: '3188/2048'}} />
